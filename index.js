57042979a39e6207037a976ca99ea548a27f8b56
arjun-88 <21951a0516@iare.ac.in>
2023-06-07 23:56:18 +0530

    sequelize

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ const app = require("./app");
 app.listen(4000, () => {
   console.log("Started express at port 3000");
 });
-//database name is sports_cal
 
-//npx sequelize-cli model:generate --name  todo --attributes title:string ,dueDate:dateOnly,completed:boolean
-//npx sequelize-cli db:migrate
